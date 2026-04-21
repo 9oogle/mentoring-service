8afa67be0c7f857c1a5b2a9ab05c168f603ee115
annieha <hajh5447@naver.com>
2026-04-21 23:42:26 +0900

    feat: init

---
+++ src/main/java/com/goggles/mentoring_server/MentoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.goggles.mentoring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoringServerApplication.class, args);
+	}
+
+}
