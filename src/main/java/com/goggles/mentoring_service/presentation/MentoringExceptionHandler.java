01d87f6f296627c2a3ae54553391ac454c469117
annieha <hajh5447@naver.com>
2026-04-23 00:27:41 +0900

    fix: handler 에러처리 확대

--- src/main/java/com/goggles/mentoring_service/presentation/MentoringExceptionHandler.java
+++ src/main/java/com/goggles/mentoring_service/presentation/MentoringExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.goggles.mentoring_service.presentation;
 
 
+import com.goggles.common.exception.ConflictException;
+import com.goggles.common.exception.ForbiddenException;
+import com.goggles.common.exception.NotFoundException;
 import com.goggles.mentoring_service.domain.exception.MentoringValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -14,6 +17,30 @@ public class MentoringExceptionHandler {
 	public ProblemDetail handleValidationException(MentoringValidationException e) {
 		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
 		problemDetail.setTitle("Invalid mentoring request");
+		problemDetail.setDetail(e.getMessage());
+		return problemDetail;
+	}
+
+	@ExceptionHandler(ConflictException.class)
+	public ProblemDetail handleConflictException(ConflictException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+		problemDetail.setTitle("Conflict");
+		problemDetail.setDetail(e.getMessage());
+		return problemDetail;
+	}
+
+	@ExceptionHandler(ForbiddenException.class)
+	public ProblemDetail handleForbiddenException(ForbiddenException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
+		problemDetail.setTitle("Forbidden");
+		problemDetail.setDetail("접근 권한이 없습니다.");
+		return problemDetail;
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	public ProblemDetail handleNotFoundException(NotFoundException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+		problemDetail.setTitle("Not Found");
 		problemDetail.setDetail(e.getMessage());
 		return problemDetail;
 	}
