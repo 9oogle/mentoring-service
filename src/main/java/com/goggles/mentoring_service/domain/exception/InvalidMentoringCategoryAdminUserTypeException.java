63941867d2b8fb18d9cf37518c635b5e49e1dffe
annieha <hajh5447@naver.com>
2026-04-23 00:27:05 +0900

    fix: NPE 방지

--- src/main/java/com/goggles/mentoring_service/domain/exception/InvalidMentoringCategoryAdminUserTypeException.java
+++ src/main/java/com/goggles/mentoring_service/domain/exception/InvalidMentoringCategoryAdminUserTypeException.java
@@ -7,6 +7,6 @@ import java.util.UUID;
 
 public class InvalidMentoringCategoryAdminUserTypeException extends ForbiddenException {
 	public InvalidMentoringCategoryAdminUserTypeException(UUID userId, UserType userType) {
-		super(String.format("권한이 없는 사용자입니다 USER ID: %s, USER_TYPE: %s", userId, userType.name()));
+		super(String.format("권한이 없는 사용자입니다. USER_TYPE: %s", String.valueOf(userType)));
 	}
 }
