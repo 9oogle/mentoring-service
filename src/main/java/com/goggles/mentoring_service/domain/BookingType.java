1bf0fdbea445a57e47271f2520efc4625b3525ac
annieha <hajh5447@naver.com>
2026-04-22 14:43:06 +0900

    feat: mentoring enums

---
+++ src/main/java/com/goggles/mentoring_service/domain/BookingType.java
@@ -0,0 +1,5 @@
+package com.goggles.mentoring_service.domain;
+
+public enum BookingType {
+	ONE_TIME, SELF_SELECT, AUTO_REPEAT
+}
---
+++ src/main/java/com/goggles/mentoring_service/domain/MentoringType.java
@@ -0,0 +1,6 @@
+package com.goggles.mentoring_service.domain;
+
+public enum MentoringType {
+	ONE_ON_ONE,
+	GROUP
+}
