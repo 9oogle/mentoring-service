92df6c393304a834f447cb42703351ea8cb6d9f6
annieha <hajh5447@naver.com>
2026-04-23 11:49:13 +0900

    fix: Category VO 설정 추가

--- src/main/java/com/goggles/mentoring_service/domain/mentoring/Category.java
+++ src/main/java/com/goggles/mentoring_service/domain/mentoring/Category.java
@@ -1,12 +1,18 @@
 package com.goggles.mentoring_service.domain.mentoring;
 
-import com.goggles.mentoring_service.domain.category.MentoringCategoryId;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Embeddable
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 	private UUID categoryId;
 	@Column(length = 50, name = "category_name")
